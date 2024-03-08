293f5dc58d5a8ee6cf216d143234283ae2b57387
@fedko-zhenja <fedkozhenja@gmail.com>
2024-03-08 18:01:13 +0400

    feat: create artist, track, album and favorites resources

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UserModule } from './user/user.module';
+// import { UserModule } from './user/user.module';
+// import { ArtistModule } from './artist/artist.module';
+// import { TrackModule } from './track/track.module';
+// import { AlbumModule } from './album/album.module';
+// import { FavoritesModule } from './favorites/favorites.module';
+import { UserModule } from './apiResources/user/user.module';
+import { ArtistModule } from './apiResources/artist/artist.module';
+import { TrackModule } from './apiResources/track/track.module';
+import { AlbumModule } from './apiResources/album/album.module';
+import { FavoritesModule } from './apiResources/favorites/favorites.module';
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    UserModule,
+    ArtistModule,
+    TrackModule,
+    AlbumModule,
+    FavoritesModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
