87a7abda3ef565ac70084f48c2ebd665cd7ecce6
@fedko-zhenja <fedkozhenja@gmail.com>
2024-03-24 01:01:51 +0400

    feat: update album service

--- src/apiResources/album/album.service.ts
+++ src/apiResources/album/album.service.ts
@@ -1,34 +1,43 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateAlbumDto } from './dto/create-album.dto';
 import { UpdateAlbumDto } from './dto/update-album.dto';
-import { DatabaseService } from 'src/database/database.service';
-import { v4 as uuidv4 } from 'uuid';
+// import { DatabaseService } from 'src/database/database.service';
+// import { v4 as uuidv4 } from 'uuid';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class AlbumService {
-  constructor(private readonly databaseSevice: DatabaseService) {}
+  constructor(private prisma: PrismaService) {}
 
-  create(createAlbumDto: CreateAlbumDto) {
-    const idValue = uuidv4();
+  async create(createAlbumDto: CreateAlbumDto) {
+    // const idValue = uuidv4();
 
-    const newDataAlbum = {
-      id: idValue,
-      ...createAlbumDto,
-    };
+    // const newDataAlbum = {
+    //   id: idValue,
+    //   ...createAlbumDto,
+    // };
 
-    this.databaseSevice.album.createData(idValue, newDataAlbum);
+    // this.databaseSevice.album.createData(idValue, newDataAlbum);
+
+    const newDataAlbum = await this.prisma.album.create({
+      data: createAlbumDto,
+    });
 
     return newDataAlbum;
   }
 
-  findAll() {
-    const albums = this.databaseSevice.album.getAllData();
+  async findAll() {
+    // const albums = this.databaseSevice.album.getAllData();
+
+    const albums = await this.prisma.album.findMany();
 
     return albums;
   }
 
-  findOne(id: string) {
-    const album = this.databaseSevice.album.getDataById(id);
+  async findOne(id: string) {
+    // const album = this.databaseSevice.album.getDataById(id);
+
+    const album = await this.prisma.album.findUnique({ where: { id } });
 
     if (!album) {
       throw new NotFoundException(`Album with id ${id} not found`);
@@ -37,41 +46,56 @@ export class AlbumService {
     return album;
   }
 
-  update(id: string, updateAlbumDto: UpdateAlbumDto) {
-    const album = this.databaseSevice.album.updateData(id);
+  async update(id: string, updateAlbumDto: UpdateAlbumDto) {
+    // const album = this.databaseSevice.album.updateData(id);
+
+    const album = await this.prisma.album.findUnique({ where: { id } });
 
     if (!album) {
       throw new NotFoundException(`Album with id ${id} not found`);
     }
 
-    album.name = updateAlbumDto.name;
-    album.year = updateAlbumDto.year;
-    album.artistId = updateAlbumDto.artistId;
+    // album.name = updateAlbumDto.name;
+    // album.year = updateAlbumDto.year;
+    // album.artistId = updateAlbumDto.artistId;
 
-    return album;
-  }
-
-  remove(id: string) {
-    const res = this.databaseSevice.album.deleteData(id);
-
-    if (!res) {
-      throw new NotFoundException(`Album with id ${id} not found`);
-    }
-
-    const tracks = this.databaseSevice.track.getAllData();
-
-    tracks.forEach((track) => {
-      if (track.albumId === id) {
-        track.albumId = null;
-      }
+    const updateAlbum = await this.prisma.album.update({
+      where: { id },
+      data: updateAlbumDto,
     });
 
-    const favoriteAlbum = this.databaseSevice.favorites.albums;
+    return updateAlbum;
+  }
 
-    if (favoriteAlbum.includes(id)) {
-      const albumIndex = favoriteAlbum.indexOf(id);
+  async remove(id: string) {
+    // const res = this.databaseSevice.album.deleteData(id);
 
-      favoriteAlbum.splice(albumIndex, 1);
+    // if (!res) {
+    //   throw new NotFoundException(`Album with id ${id} not found`);
+    // }
+
+    // const tracks = this.databaseSevice.track.getAllData();
+
+    // tracks.forEach((track) => {
+    //   if (track.albumId === id) {
+    //     track.albumId = null;
+    //   }
+    // });
+
+    // const favoriteAlbum = this.databaseSevice.favorites.albums;
+
+    // if (favoriteAlbum.includes(id)) {
+    //   const albumIndex = favoriteAlbum.indexOf(id);
+
+    //   favoriteAlbum.splice(albumIndex, 1);
+    // }
+
+    try {
+      await this.prisma.album.delete({ where: { id } });
+    } catch (error) {
+      if (error.code === 'P2025') {
+        throw new NotFoundException(`Album with id ${id} not found`);
+      }
     }
   }
 }
